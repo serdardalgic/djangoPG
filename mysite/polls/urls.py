288d4efbe80c31e9e4e34a1afd944f1e637ddd3b
Serdar Dalgıç <sd@serdardalgic.org>
2012-12-19 11:46:36 +0200

    Finished the 3rd part of djangoPG, add .ropeproject to .gitignore

---
+++ mysite/polls/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('polls.views',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+
+)
+
